38dfe63424186921be5217426d3e527c18c99dce
Luccas Mateus <luccasmmg@gmail.com>
2023-11-21 10:18:02 -0300

    Try removing all public folders

--- ckanext/sitemap/plugin.py
+++ ckanext/sitemap/plugin.py
@@ -7,12 +7,6 @@ class SitemapPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
     plugins.implements(plugins.IBlueprint)
 
-    # IConfigurer
-    def update_config(self, config_):
-        toolkit.add_template_directory(config_, "templates")
-        toolkit.add_public_directory(config_, "public")
-        toolkit.add_resource("assets", "sitemap")
-
     # IBlueprint
     def get_blueprint(self):
         return view.get_blueprints()
